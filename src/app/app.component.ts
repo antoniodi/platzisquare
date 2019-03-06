c551be0c87d965d5383acc2e812d0c700e7fcefe
antoniodi <antonio4793@hotmail.com>
2019-03-05 20:36:34 -0500

    Aplicación en Angular haciendo uso de la API de google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'platzisquare';
+  title: string = 'My first AGM project';
+  lat = 7.104217;
+  lng = -73.141460;
+  enable = false;
+  name: string;
+  lastName: string;
+  employes: any = [
+    {active: true, name: 'Ana Milena'},
+    {active: false, name: 'Laidy Rueda'},
+    {active: true, name: 'Sandra Catalina'},
+    {active: true, name: 'Elizabeth Correa'},
+    {active: true, name: 'Alejandra Rendon'}
+  ];
+
+  users: any = [
+    {name: 'Antonio Cortés', age: 24},
+    {name: 'Leidy Rueda', age: 19},
+    {name: 'Sandra rueda', age: 18},
+    {name: 'Diana Rueda', age: 17},
+    {name: 'Julian Rueda', age: 16}
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.enable = true;
+    },
+      3000);
+  }
+
+  activateButton(aName: string): boolean {
+    console.log(aName);
+    if (this.enable) {
+      this.enable = false;
+    } else {
+      this.enable = true;
+      alert('Button is activate' + aName);
+    }
+    return this.enable;
+  }
+
+  addEmploye(employesName: string) {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Library tu use two way data bindings
+import { FormsModule } from '@angular/forms';
+
+// Library to use google maps
+import { AgmCoreModule } from '@agm/core';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, AppRoutingModule, FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBUGqp0rugUGOLxr1Y5AKKjkCE2PJ5IP4A'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
